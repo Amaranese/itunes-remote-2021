b5647af15df6adfbd4835ae16df54175b936535b
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-08-28 18:46:27 +0200

    .

---
+++ node_modules/js-function-string/js-function-string.js
@@ -0,0 +1,11 @@
+module.exports = STR
+
+function STR(fn) {
+ var s = fn.toString();
+  var lines = s.split('\n').slice(1, -1)
+  var indent = /[ \t]*/.exec(lines[0])[0];
+
+  return lines.map(function(line) {
+    return line.replace(indent, '');
+  }).join('\n');
+}
